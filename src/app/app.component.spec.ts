a41191f96556b4477808e25911dda61c442c1095
taylorfernald <taylor.fernald@yahoo.com>
2025-02-25 11:39:13 -0500

    rearranged server + client to make deploying easier

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'worldbricks-server' title`, () => {
+  it(`should have the 'worldbricks' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('worldbricks-server');
+    expect(app.title).toEqual('worldbricks');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, worldbricks-server');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, worldbricks');
   });
 });
